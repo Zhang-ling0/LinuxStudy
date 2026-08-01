755976ce632fc9b8e8ef0f984aaf7a08003095db
张玲 <zl13658830776@qq.com>
2026-08-01 23:21:14 +0800

    共享内存通信

---
+++ lesson24/Shm.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include<iostream>
+#include<sys/shm.h>
+class Shm {
+    Shm()
+        {}
+    void Create() 
+    {
+        //1. 创建键值
+        //2. 创建新的共享内存
+
+    }
+    void Get() {}
+    void Attach() {}
+    void Delete() {}
+    ~Shm() {}
+}
