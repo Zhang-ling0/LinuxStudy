cdc81ddae9aafe57b423aa3ca1673fcd2b3b06ca
张玲 <zl13658830776@qq.com>
2026-08-14 23:40:04 +0800

    线程控制

--- lesson30/TestThread.cc
+++ lesson30/TestThread.cc
@@ -8,54 +8,133 @@
 #include <string>
 #include <unistd.h>
 #include <vector>
-
-int g_val = 100;
-
-void *Routine(void *args) {
-    sleep(3);
-    Task *t = static_cast<Task *>(args);
-    t->Excute();
-    std::cout << t->Result() << std::endl;
-    delete t;
-    return nullptr;  //  直接返回
+//线程局部存储
+__thread pid_t id = 0;
+void *routine(void *args) {
+  std::string name = static_cast<const char *>(args);
+  while (true) {
+    std::cout << "new thread id: " << id << std::endl;
+    printf("new thread id: %p\n",&id);
+    id++;
+    sleep(1);
+  }
+  return nullptr;
 }
 
 int main() {
-    srand(time(nullptr) ^ getpid());
-    const int num = 10;
-    std::vector<pthread_t> tids;  // 只保留 vector
-
-    for (int i = 0; i < num; i++) {
-        int x = rand() % 10 + 1;
-        usleep(234);
-        int y = rand() % 7 + 1;
-        Task *t = new Task(x, y);
-        pthread_t tid;
-        pthread_create(&tid, nullptr, Routine, t);
-        tids.push_back(tid);  // 添加 tid
-    }
-
-    // 打印所有线程 ID
-    for (auto &tid : tids) {
-        printf("tid: 0x%lx\n", tid);
-    }
-
-    // 随机取消子线程
-    while (true) {
-        printf("我是主线程tid: 0x%lx, pid: %d, g_val: %d, &g_val: %p\n",
-               pthread_self(), getpid(), g_val, &g_val);
-        sleep(1);
-
-        int who = rand() % tids.size();
-        if (tids[who] != -1) {  
-            pthread_cancel(tids[who]);
-            tids[who] = (pthread_t)-1;  // 标记为已取消
-            printf("取消线程 %d\n", who);
-        }
-    }
-
-    return 0;
+  pthread_t tid;
+  pthread_create(&tid, nullptr, routine, "thread-1");
+  while (true) {
+    std::cout << "main thread id: " << id << std::endl;
+    sleep(1);
+  }
+  return 0;
 }
+// classRes {
+// public:
+//   int code;
+//   std::string name;
+//   std::string info;
+// }
+// void *Routine(void *args) {
+//     int cnt = 5;
+//     while(cnt)
+//     {
+//         std::cout<<"新进程正在运行： "<<cnt--<<endl;
+//         sleep(1);
+//     }
+//     return nullptr;
+// //   Task *t = static_cast<Task *>(args);
+// //   t->Excute();
+// //   while(true){
+// //     sleep(1);
+//   }
+//   retuen(void *) t;
+//   std::string name = static_cast<const char *>(args);
+//   while (true) {
+//     std::cout << "new thread" << name << std::endl;
+//   }
+//   Res res = new Res();
+//   res->code = 10;
+//   res->name = name;
+//   res->info = "我这个线程已经完蛋了你们继续努力！" retuen(void *) res;
+// }
+// int main() {
+//   Task_t(10, 20);
+//   pthread_t tid;
+//   // pthread_create(&tid, nullptr, Routine, (void *)"thread-1");
+//   pthread_create(&tid, nullptr, Routine, (void *)t);
+//   sleep(1);
+//   stdcout << "分离线程"<<std::endl;
+//    pthread_cancel(tid);
+// //   std::cout << "取消目标线程：    "<<std::endl;
+// //   pthread_cancel(tid);
+//   //Task *task;
+//   // 1. 一般而言不许等待新线程退出，如果不等待，导致类似僵尸的问题
+//   // 2.为了获取新线程的执行解耦
+//   // res *retval = nullptr;
+//   // 为什么这里就直接是新线程的退出信息，没有退出信号？没有异常分析？
+//   //int n = pthread_join(tid, (void **)&retval);
+//   void *ret = nullptr;
+//   int n = pthread_join(tid,&ret);
+//   if (n == 0) {
+//     std::cout << "join success: " << (long long)ret << std::endl;
+//   }
+//   else
+//   {
+//     std::cout <<"join error: "<<n<<std::endl;
+//   }
+//   delete retval;
+//   return 0;
+// }
+
+// int g_val = 100;
+
+// void *Routine(void *args) {
+//     sleep(3);
+//     Task *t = static_cast<Task *>(args);
+//     t->Excute();
+//     std::cout << t->Result() << std::endl;
+//     delete t;
+//     return nullptr;  //  直接返回
+// }
+
+// int main() {
+//     srand(time(nullptr) ^ getpid());
+//     const int num = 10;
+//     std::vector<pthread_t> tids;  // 只保留 vector
+
+//     for (int i = 0; i < num; i++) {
+//         int x = rand() % 10 + 1;
+//         usleep(234);
+//         int y = rand() % 7 + 1;
+//         Task *t = new Task(x, y);
+//         pthread_t tid;
+//         pthread_create(&tid, nullptr, Routine, t);
+//         tids.push_back(tid);  // 添加 tid
+//     }
+
+//     // 打印所有线程 ID
+//     for (auto &tid : tids) {
+//         printf("tid: 0x%lx\n", tid);
+//     }
+
+//     // 随机取消子线程
+//     while (true) {
+//         printf("我是主线程tid: 0x%lx, pid: %d, g_val: %d, &g_val: %p\n",
+//                pthread_self(), getpid(), g_val, &g_val);
+//         sleep(1);
+
+//         int who = rand() % tids.size();
+//         if (tids[who] != -1) {
+//             pthread_cancel(tids[who]);
+//             tids[who] = (pthread_t)-1;  // 标记为已取消
+//             printf("取消线程 %d\n", who);
+//         }
+//     }
+
+//     return 0;
+// }
 // void PrintName(const std::string &name) {
 //   printf("我是新线程tid: %s, tid 0x%lx, pid: %d\n", name.c_str(),
 //          pthread_self(), getpid());
@@ -102,10 +181,11 @@ int main() {
 //     usleep(234);
 //     int y = rand()%7+1;
 //     Task *t = new Task(x,y);
-//     pthread_create(&tid, nullptr,Routine, t); // 每一次拿到的就只是开始缓冲区的地址
+//     pthread_create(&tid, nullptr,Routine, t); //
+//     每一次拿到的就只是开始缓冲区的地址
 //     //sleep(1);
 //   }
- 
+
 // //   while (true) {
 // //     printf("我是主线程tid: 0x%lx, pid: %d, g_val: %d, &g_val: %p\n",
 // //            pthread_self(), getpid(), g_val, &g_val);
