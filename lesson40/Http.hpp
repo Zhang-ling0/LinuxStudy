16c4570ba2ba98a1f9c8bea36ffa331021da0f3b
张玲 <zl13658830776@qq.com>
2026-09-14 20:27:31 +0800

    HTTP表单

--- lesson40/Http.hpp
+++ lesson40/Http.hpp
@@ -5,14 +5,19 @@
 #include "Logger.hpp"
 #include <fstream>
 #include <unordered_map> //结构化数据
+#include <sys/stat.h>
+#include <functional>
+#include <cstring>
 
 const std::string linesep = "\r\n";
 const std::string spacesep = " ";
 const std::string headersep = ": ";
+const std::string suffixsep = ".";
+const std::string argsep = ".";
 const std::string g_http_version = "HTTP/1.1";
 const std::string g_first_page = "index.html";
 const std::string g_wwwroot = "wwwzhangling";
-const std::string page_404 ="./wwwzhangling/404.html";
+const std::string page_404 = "./wwwzhangling/404.html";
 using namespace NS_LOG_MODULE;
 class Util
 {
@@ -32,21 +37,37 @@ public:
         return line;
     }
 
+    // 如果我们要读取二进制数据，使用vector的形式接收
     static std::string ReadFile(const std::string &filename)
     {
         // 读取文件
         // bug
-        std::ifstream in(filename, std::ios::binary);
+        // std::ifstream in(filename, std::ios::binary);
+        // if (!in.is_open())
+        // {
+        //     return std::string();
+        // }
+        // // 读取文件内容
+        // std::string content;
+        // // in>>content;//将文件内容重定向到这里,按空格截取错误
+        // std::string line;
+        // while (std::getline(in, line))
+        //     content += line;
+        // in.close();
+        // return content;
+        // 采用二进制方式读取数据--设计一个buffer类（比较麻烦，我们在这里使用string)
+
+        std::ifstream in(filename, std::ios::binary); // 以二进制方式打开文件
         if (!in.is_open())
-        {
-            return std::string();
-        }
-        // 读取文件内容
+            return "";
+        in.seekg(0, in.end); // 设置文件读取位置
+        // ftat(系统调用) fseek
+        int filesize = in.tellg();
+        in.seekg(0, in.beg);
+        // 设计一个缓冲区
         std::string content;
-        // in>>content;//将文件内容重定向到这里,按空格截取错误
-        std::string line;
-        while (std::getline(in, line))
-            content += line;
+        content.resize(filesize);                   // 设置大小
+        in.read((char *)content.c_str(), filesize); // 读取数据
         in.close();
         return content;
     }
@@ -68,6 +89,19 @@ private:
             _uri += g_first_page; // 将首页发送过去
         // 不要让它去跟目录下找，从www文件下去找
         _uri = g_wwwroot + _uri; // wwwzhangling/index.html:web根目录
+
+        // wwwzhangling .html .css .js .jpg等各种文件
+        // 倒着搜索一个点，就是文件的后缀
+        auto pos = _uri.rfind(suffixsep); // 倒着搜索
+        if (pos == std::string::npos)     // 没找到
+            _suffix = ".html";
+        else
+        {
+            _suffix = _uri.substr(pos);
+
+            return true;
+        }
+
         return true;
     }
     bool ParseHeaderkv(std::string &httpstr)
@@ -97,17 +131,42 @@ private:
         return true;
     }
 
-    bool ParseText(std::string httpstr) // 默认读到一个完整的报文
+    bool ParseText(std::string &httpstr) // 默认读到一个完整的报文
     {
         // 1.http请求有没有正文(还要结合方法)
-        if (_header.find("Content-Length") == _header.end())
+        if(strcasecmp(_method.c_str(),"GET")==0)//字符串比较函数，忽略大小写时候的比较
+        {
+            //分析处理GET方法
+            //确认你的Get方法有没有带参数，看你的url里有没有？判断你是单纯的获取还是带参数
+            //没带参就是常规的请求
+            auto pos = _uri.find(argsep);
+            if(pos==std::string::npos)
+            {
+                
+                _text=std::string();
+                return true;//没有问号不带参数
+            }
+            else
+            {
+                std::cout<<"_URI: "<<_uri<<std::endl;
+                _uri=_uri.substr(0,pos);
+                std::string temp =_uri;//临时保存一下
+                std::cout<<"_URI: "<<_uri<<std::endl;
+                //提取子串
+                _text=temp.substr(pos+argsep.size());//参数设置到正文部分
+                //命令行参数转到正文里，命令行参数在处理时，可以通过读取正文拿到参数
+            }
+
+        }
+        else
+        {
+            if (_header.find("Content-Length") == _header.end())
         {
             // 没有属性
             _text = "";
         }
-        else
-        {
             // 2.正文多大，有没有正文
+            //给post方法提取正文的
             int content_len = std::stoi(_header["Content-Length"]);
             _text = httpstr.substr(0, content_len);
             httpstr.erase(0, content_len);
@@ -128,12 +187,12 @@ public:
         LOG(LogLevel::DEBUG) << "_uri# " << _uri;
         LOG(LogLevel::DEBUG) << "_version# " << _version;
         // 2.解析报头:文本处理
-        // n = ParseHeaderkv(httpstr); // 将数据取出来并插入到报头中
-        // for (auto &header : _header)
-        // {
-        //     std::cout << header.first << "##" << header.second << "\r\t\n";
-        // }
-        //std::cout << "start|" << httpstr << "|end";
+        n = ParseHeaderkv(httpstr); // 将数据取出来并插入到报头中
+        for (auto &header : _header)
+        {
+            std::cout << header.first << "##" << header.second << "\r\t\n";
+        }
+        std::cout << "start|" << httpstr << "|end";
         _blank_line = linesep;
         // 3.解析正文部分
         n = ParseText(httpstr);
@@ -145,7 +204,28 @@ public:
     {
         return Util::ReadFile(_uri);
     }
+    std::string Suffix()
+    {
+        return _suffix;
+    }
     // 反序列化，不依赖任何库的序列化和反序列化协议
+    void DebugPrint()
+    {
+        std::cout << "_version" << _version << std::endl;
+        std::cout << "_uri" << _uri << std::endl;
+        std::cout << "_method" << _method << std::endl;
+        for (auto &header : _header)
+        {
+            std::cout << "->" << header.first << " # " << header.second << std::endl;
+        }
+        std::cout << _blank_line << std::endl;
+        std::cout << _text << std::endl;
+    }
+    std::string Text()
+    {
+        return _text;
+    }
+
 private:
     std::string _method;
     std::string _uri;     // 如果是裸的\默认把首页带上
@@ -153,6 +233,9 @@ private:
     std::unordered_map<std::string, std::string> _header;
     std::string _blank_line; // 空行
     std::string _text;       // 表示你的正文
+
+    // 私有数据
+    std::string _suffix; // 请求资源的后缀类型
 };
 
 class HttpResponse
@@ -239,12 +322,12 @@ public:
     }
     int BodySize()
     {
-        return _text.size();
+        return _text.size();//文本长度
     }
-    void AddHeader(const std::string &key,const std::string &value)
+    void AddHeader(const std::string &key, const std::string &value)
     {
-        //动态添加报头，如果存在就覆盖，不存在就添加
-        _header[key]=value;   
+        // 动态添加报头，如果存在就覆盖，不存在就添加
+        _header[key] = value;
     }
     // bool Build(HttpRequest &req)
     // {
@@ -293,7 +376,38 @@ private:
     std::string _blank_line;
     std::string _text;
 };
+// 服务类型
+using service_t = std::function<void(HttpRequest &req, HttpResponse &resp)>;
+class MiniType
+{
+public:
+    static std::string Suffix2MimeType(const std::string &suffix)
+    {
+        auto iter = _mine_map.find(suffix);
+        if (iter != _mine_map.end())
+            return iter->second;
+        else
+            return "text/html";
+    }
 
+private:
+    static std::unordered_map<std::string, std::string> _mine_map; // 将服务进行映射
+};
+std::unordered_map<std::string, std::string> MiniType::_mine_map =
+    {
+        // 类内函数，类外初始化--静态成员
+        {".html", "text/html; charset=utf-8"},
+        {".htm", "text/html; charset=utf-8"},
+        {".css", "text/css; charset=utf-8"},
+        {".js", "application/javascript; charset=utf-8"},
+        {".json", "application/json; charset=utf-8"},
+        {".png", "image/png"},
+        {".jpg", "image/jpeg"},
+        {".jpeg", "image/jpeg"},
+        {".gif", "image/gif"},
+        {".ico", "image/x-icon"},
+        {".mp4", "video/mp4"},
+};
 class HttpProtocol
 {
 public:
@@ -301,6 +415,20 @@ public:
     {
     }
 
+    void RegisterService(const std::string &uri, service_t service) // 向http注册服务
+    {
+        std::string key = g_wwwroot + uri;
+
+        _http_services[key] = service; // key->wwwzhangling/Login
+    }
+    bool IsReqService(const std::string &uri)
+    {
+        auto iter = _http_services.find(uri); // 判断用户的uri是否提交
+        if (iter == _http_services.end())     // 没有找到
+            return false;
+        else
+            return true;
+    }
     std::string HandlerHttpRequest(std::string &req)
     {
 #ifdef DEBUG
@@ -324,34 +452,53 @@ public:
 
         // 5. 拼接：状态行 + 头部 + 空行 + body
         return status_line + headers + blank_line + html;
+        b
 #endif
-        // 1. 保证报文的完整性--暂时忽略
-        // 2. 反序列化
+            // 判断申请的是不是静态资源还是动态资源，看用户是不是在请求某一个服务
         HttpRequest http_req;
         http_req.Deserialize(req);
-        // 3. 根据不同的请求方法和请求URI，给用户返回不同的报文--构建应答
         HttpResponse http_resp;
-        std::string content= http_req.RequestContent();
-        if(content.empty())
+
+        if (IsReqService(http_req.Uri())) // 服务处理--看url是否在目录里
         {
-            // //为空没找到404，正文不存在
-            // http_resp.SetCode(200);
-            // //404也是要对应内容页面的
-            // //重新读取一下新的网页内容
-            // http_resp.SetBody(Util::ReadFile(page_404));
-            // http_resp.AddHeader("Content-Length",std::to_string(http_resp.BodySize()));
-            http_resp.SetCode(302);
-            //http_resp.AddHeader("Location","https://www.baidu.com/");//重定向，让浏览器二次请求
-            http_resp.AddHeader("Location","/404.html");//只要访问失败了，都可以进行一个重定向将所有的错误都归结在一个页面里
+            // 如何判断？注册服务用户请求的uri进行判断,找到之后对其进行回调
+            // 功能路由--给上层
+            _http_services[http_req.Uri()](http_req, http_resp); // 真正确定的是uri的拼接部分
         }
         else
         {
-            http_resp.SetCode(200);//为应答设置状态码
-            http_resp.AddHeader("Content-Length",std::to_string(content.size()));
-            http_resp.SetBody(content);
+            // 1. 保证报文的完整性--暂时忽略
+            // 2. 反序列化
+            // std::cout<<"handler pid:"<<getgid()<<std::endl;
+            // std::cout<<req<<std::endl;
+
+            // std::cout << "Suffix: " << http_req.Suffix() << std::endl;
+            //  3. 根据不同的请求方法和请求URI，给用户返回不同的报文--构建应答
+            std::string content = http_req.RequestContent();
+            if (content.empty())
+            {
+                // //为空没找到404，正文不存在
+                // http_resp.SetCode(200);
+                // //404也是要对应内容页面的
+                // //重新读取一下新的网页内容
+                // http_resp.SetBody(Util::ReadFile(page_404));
+                // http_resp.AddHeader("Content-Length",std::to_string(http_resp.BodySize()));
+                http_resp.SetCode(302); // 重新请求并 定向到404页面
+                // http_resp.AddHeader("Location","https://www.baidu.com/");//重定向，让浏览器二次请求
+                http_resp.AddHeader("Location", "/404.html"); // 只要访问失败了，都可以进行一个重定向将所有的错误都归结在一个页面里
+            }
+
+            else
+            {
+                http_resp.SetCode(200); // 为应答设置状态码
+                http_resp.AddHeader("Content-Length", std::to_string(content.size()));
+                http_resp.AddHeader("Content-Type", MiniType::Suffix2MimeType(".txt"));//将特定后缀转化为特定格式
+                http_resp.AddHeader("Content", "close");
+                http_resp.SetBody(content);
+            }
+            // http_resp.Build(http_req);
         }
-        //http_resp.Build(http_req);
-        return http_resp.Serialize();//序列化
+        return http_resp.Serialize(); // 序列化
     }
 
     ~HttpProtocol()
@@ -359,4 +506,5 @@ public:
     }
 
 private:
+    std::unordered_map<std::string, service_t> _http_services;//注册服务的接口   
 };
--- lesson40/HttpServer.cc
+++ lesson40/HttpServer.cc
@@ -8,6 +8,35 @@ static void Usage(const std::string &proc)
 {
     std::cout << "Usage::\n\t" << proc << " port" << std::endl;
 }
+void Login(HttpRequest& req,HttpResponse& resp)
+{
+    std::cout<<"\nLogin function been called!"<<std::endl;
+    req.DebugPrint();//将服务器中的内容注册到这个服务里
+    std::string data=req.Text();
+    std::cout<<"data is: "<<data<<std::endl;
+    //根据分割符将数据提取出来
+    //访问数据库，查找数据库,确认数据是存在的，设置应答
+    resp.SetCode(200);
+    resp.AddHeader("Content-Type",".txt");
+    //resp.SetBody("{\"statuscode\":\"0\"}");
+    resp.SetBody("Login success!");
+    resp.AddHeader("Content-Length",std::to_string(resp.BodySize()));
+}
+void Register(HttpRequest& req,HttpResponse& resp)
+{
+    //插入数据就变成了register
+    
+    
+}
+void GetProductList(HttpRequest& req,HttpResponse& resp)//服务会根据功能路由路由过来
+{
+
+}
+void Search(HttpRequest& req,HttpResponse& resp)
+{
+
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -28,6 +57,11 @@ int main(int argc, char *argv[])
         {
             return protocol->HandlerHttpRequest(inbuffer);
         },port);
+        //向Http中注册服务,
+    protocol->RegisterService("/Login",Login);//注册服务
+    protocol->RegisterService("/Register",Register);//注册服务
+    protocol->RegisterService("/Search",Search);//注册服务
+    protocol->RegisterService("/api/getproductlist",GetProductList);//注册服务
 
     // 启动
     tsvr->Loop();
