1cc1443d539b3c57036b9c154800b3586780b2d0
张玲 <zl13658830776@qq.com>
2026-09-22 21:19:22 +0800

    C++特性测试

---
+++ lesson42/text.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+int main()
+{
+    auto add1 = [](int x,int y)->int{return x+y;};
+    std::cout << add1(1,2)<<std::endl;
+    //1. 捕捉为空也不能省略
+    //2. 参数为空可以省略
+    //3. 返回值可以省略，可以通过返回对象自动推导
+    //4. 函数体不能省略
+    auto add2=[]{
+        std::cout<<"hhhhhh"<<std::endl;
+        return 0;
+    };
+    add2();
+    return 0;
+}
